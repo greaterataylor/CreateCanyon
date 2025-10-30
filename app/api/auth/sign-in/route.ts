24cafd52adf5ac347836240294178f7022b16ac2
greaterataylor <147682168+greaterataylor@users.noreply.github.com>
2025-10-30 11:18:15 +0800

    Update route.ts

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -1,3 +1,4 @@
+//app/api/auth/sign-in/route.ts
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { verifyPassword, createSessionCookie } from '@/lib/auth'
