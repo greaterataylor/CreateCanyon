64db68324cd7a9b7d5a98b03bc51cd5e38f7494a
greaterataylor <147682168+greaterataylor@users.noreply.github.com>
2025-10-30 14:55:40 +0800

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,4 @@
-//lib/stripe.ts
-import Stripe from 'stripe'
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', { apiVersion: '2024-06-20' })
+// lib/stripe.ts
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '');
