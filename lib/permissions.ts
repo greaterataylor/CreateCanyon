fa29553d32eac9d78daae10610bdf6887aa2bb62
greaterataylor <147682168+greaterataylor@users.noreply.github.com>
2026-03-30 17:20:05 +0800

    Delete lib directory

--- lib/permissions.ts
+++
@@ -1,5 +0,0 @@
-//lib/permissions.ts
-import { AssetStatus, VendorStatus } from '@prisma/client'
-
-export const canSeeAsset = (status: AssetStatus) => status === 'APPROVED'
-export const canUploadIfVendorApproved = (vendorStatus?: VendorStatus | null) => vendorStatus === 'APPROVED'
